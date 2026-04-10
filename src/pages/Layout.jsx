a7c470fdd096fa1ba90e2d53bad4ff928fb838be
liku9 <152514803+liku9@users.noreply.github.com>
2026-04-10 09:18:35 +0530

    Layout file added

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import { Outlet } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import appStore from '../utils/appStore'
+const Layout = () => {
+  return (
+    <Provider store={appStore}>
+        <Header/>
+        <Outlet/>
+    </Provider>
+  )
+}
+
+export default Layout
